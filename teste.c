1e4111fcb74976f8664b39e07f3f2135c1f79dbc
Vinicius Oliveira <35852229+vinioo@users.noreply.github.com>
2019-09-10 22:10:46 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -4,71 +4,84 @@
 
 void menu();
 int getNumber();
-float getFloatPositiveNumber();
-float calcPaymentValue(int days);
+void getFloatPositiveNumber(float *numero);
+void calcPaymentValue(int workDays, float *salarioFinal);
 float calcPayment(float salary);
 float calcAverageSalary(float salary[]);
 
 int main() {
-  char option[1];
+
   int workDays;
-  float newSalary;
-  do {
+  float newSalary, *pontNewSalary;
+  pontNewSalary = &newSalary;
+
     menu();
-    scanf("%s", &option);
-  } while (strcmp(option, "C") || strcmp(option, "A") || strcmp(option, "B"));
-  // Seria essa a melhor forma para comparação de strings no C?
 
-  if (option == "A") {
-    workDays = getNumber();
     // Como passar o parametro de salario por referencia aqui
-    newSalary = calcPaymentValue(workDays);
-    printf("\n O valor do pagamento será de %f", newSalary);
-  }
 
-  if (option == "B") {
-    newSalary = calcPayment(newSalary);
-    printf("\n O valor do salario será de %f", newSalary);
-  }
+	printf("Digite o numero de dias trabalhados");
+	workDays = getNumber();
+
+    calcPaymentValue(workDays, pontNewSalary);
+    printf("\n O valor do pagamento será de %f", newSalary);
+
+
+//
+//    newSalary = calcPayment(newSalary);
+//    printf("\n O valor do salario será de %f", newSalary);
+
   getNumber();
   return 0;
 }
 
 void menu() {
-  printf("\n A- Resultado do exercício 04 \n B- Resultado do exercício 5 \n C- Finalizar");
+  int workDays;
+  float realNumber, *pontRealNumber;
+  pontRealNumber = &realNumber;
+	char option;
+    scanf("%c", &option);
+	do {
+		printf("\n A- Resultado do exercício 04 \n B- Resultado do exercício 5 \n C- Finalizar");
+		switch (option) {
+			case 'A':
+			    workDays = getNumber();
+			    printf("\n O numero digitado foi de %d", workDays);
+				break;
+			case 'B':
+				getFloatPositiveNumber(pontRealNumber);
+				printf("\n O numero digitado foi %f", realNumber);
+				break;
+			case 'C':
+				break;
+		}
+	} while (option != 'C');
 }
 
 int getNumber() {
   int number;
-
-  do {
-  printf("\n Digite um número inteiro positivo ou nulo:");
-  scanf("%d", &number);
-    if (number < 0 && number != '\0') {
-      printf("\n O número não pode ser negativo!");
-    }
-  } while (number < 0 && number != '\0');
-
-  return number;
-}
-
-float getFloatPositiveNumber() {
-  float number;
-  do {
-  printf("\n Digite um número real positivo:");
-  scanf("%f", &number);
-    if (number < 0) {
-      printf("\n O número não pode ser negativo!");
-    }
-  } while (number < 0);
+	  do {
+	  printf("\n Digite um número inteiro positivo ou nulo:");
+	  scanf("%d", &number);
+		if (number < 0 && number != '\0') {
+		  printf("\n O número não pode ser negativo!");
+		}
+	  } while (number < 0 && number != '\0');
 
   return number;
 }
 
-float calcPaymentValue(int workDays) {
+void getFloatPositiveNumber(float *numero) {
+	  do {
+	  printf("\n Digite um número real positivo:");
+	  scanf("%f", numero);
+	  	  if(*numero <= 0) {
+	  		  printf("Valor invalido");
+	  	  }
+	  } while (*numero <= 0);
+}
 
-  int newSalary = (workDays * 130) * 0.92;
-  return newSalary;
+void calcPaymentValue(int workDays, float *salarioFinal) {
+	*salarioFinal = (workDays * 130) * 0.92;
 }
 
 float calcPayment(float salary) {
