fa61547b35425c41693d652d0fb3071b6523b1bc
Vinicius Oliveira <35852229+vinioo@users.noreply.github.com>
2019-11-26 22:10:34 -0300

    Update biblioteca.c

--- ED1 (c)/duplamenteEncadeada/biblioteca.c
+++ ED1 (c)/duplamenteEncadeada/biblioteca.c
@@ -11,10 +11,10 @@
 #include "listaDuplamenteEncadeada.h"
 
 codigos* inserirInicio(codigos *lista) {
-	//Alocar novo espaço
+	//Alocar novo espaÃ§o
 	codigos *novo = malloc (sizeof(codigos));
 
-	//Preencher o campos básicos
+	//Preencher o campos bÃ¡sicos
 	novo->codigo = RANDOM_NUMBER;
 		novo->proximo = lista;
 		novo->anterior = NULL;
@@ -29,21 +29,63 @@ codigos* inserirInicio(codigos *lista) {
 }
 
 codigos* inserirFimLista(codigos *lista) {
-	//Alocar novo espaço
+	//Alocar novo espaÃ§o
 	codigos *novo = (codigos*) malloc (sizeof(codigos));
 
-	//Preencher o campos básicos
+	//Preencher o campos bÃ¡sicos
 	novo->codigo = RANDOM_NUMBER;
 	novo->proximo = NULL;
 
 	if (lista == NULL){
+		novo->anterior = NULL;
 		return novo;
 	} else {
 		codigos *tmp = lista;
-		while (tmp->codigo != NULL) {
+		while (tmp->proximo != NULL) {
 			tmp = tmp->proximo;
 		}
 		tmp->proximo = novo;
+		novo->anterior = tmp;
 		return lista;
 	}
+}
+
+codigos* excluirLista(codigos *lista, int numero) {
+	codigos *posicaoAtual = lista;
+
+	while(posicaoAtual != NULL) {
+
+		// primeiro
+		if (lista->codigo == numero && lista->anterior == NULL) {
+
+			posicaoAtual->proximo = posicaoAtual->proximo->proximo;
+			posicaoAtual->proximo->anterior = NULL;
+
+			free(lista);
+
+			return posicaoAtual;
+		}
+
+//		// fim
+		if (lista->codigo == numero && lista->proximo == NULL) {
+
+			posicaoAtual->anterior =
+		}
+
+		// meio
+		if (lista->codigo == numero) {
+
+			posicaoAtual->anterior->proximo = posicaoAtual->proximo;
+			posicaoAtual->proximo->anterior = posicaoAtual->anterior;
+
+			free(lista);
+
+			return posicaoAtual;
+		}
+
+
+	}
+
+
+
 }
